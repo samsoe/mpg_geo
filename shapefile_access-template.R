20ba87d3f737868bb74303060eb48e343da49e1f
Erik Samsoe <esamsoe@gmail.com>
2022-12-07 12:12:40 -0700

    Update shapefile_access-template.R

--- shapefile_access-template.R
+++ shapefile_access-template.R
@@ -3,9 +3,6 @@ library(mapview)
 library(dplyr)
 library(ggplot2)
 
-# GeoDatabase
-gd_url <- "https://storage.cloud.google.com/mpg-data-warehouse/mpg_geo/GeoDatabase/DATA_SHARE_TEST_GDB.zip"
-
 # Shapefile
 shp_url <- "https://storage.googleapis.com/mpg-data-warehouse/mpg_geo/Shapefile/Test_Shapefile/Revegetation_All_Years_Test.shp"
 
