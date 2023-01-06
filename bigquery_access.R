2340def08527051ef4362259f162e4f253d32401
Erik Samsoe <esamsoe@gmail.com>
2023-01-06 07:32:20 -0700

    Update BigQuery access Script

--- bigquery_access.R
+++ bigquery_access.R
@@ -2,17 +2,25 @@ library(bigrquery)
 library(dplyr)
 library(ggplot2)
 
-# Query for data in BigQuery
-sql_query <- "SELECT * FROM `mpg-data-warehouse.geo.reveg_test`"
+# BigQuery tables in mpg-data-warehouse.geo
+veg_man <- "vegetation_management"
+spray <- "spraying"
+reveg <- "revegetation"
 
+select_category <- veg_man
+
+# SQL Query for data in BigQuery
+sql_query <- paste0("SELECT * FROM `mpg-data-warehouse.geo.", select_category, "`")
+
+# Pull data and load into dataframe
 df_bq <- bq_project_query('mpg-data-warehouse', sql_query) %>%
   bq_table_download() %>%
   as.data.frame()
 
+# This might not be necessary
 d <- st_as_sf(df_bq, wkt = "geometry", crs=4326)
 
-d %>% str()
+d %>% glimpse()
 
 ggplot() +
-  geom_sf(data = d, aes(fill = as.factor(Season))) +
-  facet_wrap(~Year)
+  geom_sf(data = d, aes(fill = as.factor(Type)))
